ed29734c04c21c8bdeaebdb4f82f167c0a65cbd5
KovalenkoAV <merlinwt@gmail.com>
2018-07-14 14:29:34 +0300

    Add calculator
    :empty:
    
    Class which will use to learn unit tests
    
    @Lib:calculator

---
+++ lib/Calculator.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kov
+ * Date: 14.07.18
+ * Time: 13:27
+ */
+class Calculator
+{
+	
+	public function sum($val1, $val2) {
+		return $val1 + $val2;
+	}
+	
+	public function multiply(...$args) {
+		$result = 1;
+		foreach ($args as $val) {
+			$result *= $val;
+		}
+		
+		return $result;
+	}
+	
+}
